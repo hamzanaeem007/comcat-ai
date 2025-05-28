5a836b6ccdab02e09d77ac885f262281463a47cf
hamzanaeem007 <official.hamzanaeem@gmail.com>
2025-05-28 21:54:01 +0500

    design changes

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -13,11 +13,14 @@ export default function About() {
           </div>
           <div className="col-lg-7">
             <div className="about-content">
-              <span className="icon"><Image src="/images/logo-icon.webp" alt="" width={20} height={20}/>ABOUT US</span>
+              <span className="icon">
+                <Image src="/images/logo-icon.webp" alt="" width={20} height={20} />
+                ABOUT US
+              </span>
               <h2>What is Comcat Digital?</h2>
               <div className="about-paragraph-wrapper">
                 <p>
-                  ComCat Digital is an AI digital signage software solution, designed for businesses that require centralized control over multiple display screens. Whether you're operating a single location or a nationwide network, our platform ensures real-time content updates with accuracy and efficiency.
+                  ComCat Digital is an AI digital signage software solution, designed for businesses that require centralized control over multiple display screens. Whether you&apos;re operating a single location or a nationwide network, our platform ensures real-time content updates with accuracy and efficiency.
                 </p>
                 <p>
                   With advanced features like remote content management, scheduling, and easy scalability, ComCat Digital enhances audience engagement while simplifying operations.
